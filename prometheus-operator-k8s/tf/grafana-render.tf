9441b2dd4d0f42a8588f18d0591ce51cea6b128c
zmarth <mazur.aleksandru@gmail.com>
2024-01-22 23:56:25 +0300

    added tf directory for  prometheus-operator-k8s

---
+++ prometheus-operator-k8s/tf/grafana-render.tf
@@ -0,0 +1,36 @@
+data "helm_template" "grafana" {
+  name = "grafana"
+
+  repository        = "https://grafana.github.io/helm-charts"
+  chart             = "grafana"
+  namespace         = "monitoring"
+  create_namespace  = true
+  version           = "6.50.7"
+
+  set {
+    name    = "image.tag"
+    value   = "9.3.6"
+  }
+
+  set {
+    name    = "adminPassword"
+    value   = "devops123"
+  }
+
+  set {
+    name    = "persistance.enabled"
+    value   = "true" 
+  }
+
+  set {
+    name    = "persistance.size"
+    value   = "8Gi" 
+  }
+}
+
+resource "local_file" "grafana_manifest" {
+  for_each = data.helm_template.grafana.manifests
+
+  filename = "target/${each.key}"
+  content  = each.value
+}
